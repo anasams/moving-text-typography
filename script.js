4316c897a23d40fd89200e1f570ea30833f03cc9
anasams <122623509+anasams@users.noreply.github.com>
2023-01-15 22:13:06 +0100

    added p5.min, and JS

---
+++ script.js
@@ -0,0 +1,42 @@
+let font
+let graphic
+letgraphic2
+
+
+function preload () {
+font = loadFont("spacegrotesk-medium.otf")
+}
+
+
+function setup() {
+createCanvas(500, 500)
+
+graphic = createGraphics(500, 500)
+
+graphic.textFont(font)
+graphic.textSize(400)
+graphic.fill("#2fff40")
+graphic.textAlign(CENTER, CENTER)
+graphic.text("no", width / 2, height / 2)
+
+graphic2 = createGraphics(500, 500)
+
+graphic2.textFont(font)
+graphic2.textSize(400)
+graphic2.fill("#0000ff")
+graphic2.textAlign(CENTER, CENTER)
+graphic2.text("ja", width / 2, height / 2)
+
+
+
+
+}
+
+function draw() {
+    background("#ef5ddc")
+
+    let val = sin(frameCount * 0.02) * 250
+
+    copy(graphic, 0, 0, 250 + val, 500, 0, 0, 250 + val, 500)
+    copy(graphic2, 250 + val, 0, 250 - val, 500, 250 + val, 0, 250 - val, 500)
+}
